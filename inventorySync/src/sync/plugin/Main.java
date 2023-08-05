86060c6131c009d690146a52306bae89aa9bdaed
BuildTools <unconfigured@null.spigotmc.org>
2023-08-06 01:01:11 +0200

    upload

---
+++ inventorySync/src/sync/plugin/Main.java
@@ -0,0 +1,72 @@
+package sync.plugin;
+
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Particle.DustOptions;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapView;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+
+
+
+
+public class Main extends JavaPlugin {
+
+	
+	
+	public static Inventory inv;
+	private static Main instance;
+	public static Main getInstance() {
+		return instance;
+	}
+	
+
+	public void onEnable() {
+		instance = this;
+		getServer().getPluginManager().registerEvents(new Listeners(this), this);
+		inv = Bukkit.createInventory(null, InventoryType.PLAYER, "Shared Inventory");
+
+
+	}
+		
+
+	public void onDisable() {
+		
+		instance = null;
+	}
+	
+	boolean cando = true;
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args) {
+        if (command.getName().equalsIgnoreCase("command")) {
+            sender.sendMessage("This can be changed");
+            return true;
+        }
+        //Add commands here
+        
+        
+        
+        return false;
+    }
+
+}
